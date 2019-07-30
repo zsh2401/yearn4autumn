cc07573798b7bd84000c94eaec213e63e4f3976f
zsh2401 <zsh2401@163.com>
2019-07-31 02:58:27 +0800

    主页也支持新的打包逻辑了

---
+++ src/view/rpage/index/page.config.ts
@@ -0,0 +1,10 @@
+import IPageConfig from "../../../common/buildV2/IPageConfig";
+
+let config:IPageConfig = {
+    entry_path:"index.tsx",
+    output:"index.html",
+
+    title:"慕秋-什么都有",
+    desc:"慕秋-什么都有"
+}
+export default config;
