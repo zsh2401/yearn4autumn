315e8768952bcaecae64da4c69540ff143a6e989
zsh2401 <zsh2401@163.com>
2019-07-31 14:41:23 +0800

    使用bootstrap4

--- src/view/template/StdPage.tsx
+++ src/view/template/StdPage.tsx
@@ -10,23 +10,28 @@ export class StdPage extends React.Component<StdPageProps>{
             <title>{this.props.title}</title>
             <meta charSet='utf-8'></meta>
             <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+            {/* 前置库 */}
             <script src="https://cdn.bootcss.com/moment.js/2.24.0/moment.min.js"></script>
             <script src="https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js"></script>
 
+            {/* react */}
             <script src="https://cdn.bootcss.com/react/16.8.6/umd/react.production.min.js"></script>
             <script src="https://cdn.bootcss.com/react-dom/16.8.6/umd/react-dom.production.min.js"></script>
 
-        
+            {/* antd */}
             <link href="https://cdn.bootcss.com/antd/3.20.7/antd.min.css" rel="stylesheet"></link>
             <script src="https://cdn.bootcss.com/antd/3.20.7/antd.min.js"></script>
 
+            {/* valine */}
             <script src="https://cdn.jsdelivr.net/npm/leancloud-storage@3.14.1/dist/av-min.js"></script>
             <script src="https://cdn.jsdelivr.net/npm/valine@1.3.9/dist/Valine.min.js"></script>
 
-            <link href="https://cdn.bootcss.com/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"></link>
-            <script src="https://cdn.bootcss.com/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+            {/* <link href="https://cdn.bootcss.com/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"></link> */}
+            {/* <script src="https://cdn.bootcss.com/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script> */}
+            <link href="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet"></link>
+            <script src="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
             <script>
-                var fuck = {JSON.stringify(this.props.extraData)}
+                var ext_data = {JSON.stringify(this.props.extraData)}
             </script>
             <body>
                 {this.props.children}
