00b71de2491693bcc6029c19e8fa68eb7500280e
zsh2401 <zsh2401@163.com>
2019-08-04 00:19:52 +0800

    进一步完善布局

--- src/view/controls/AppLayout.tsx
+++ src/view/controls/AppLayout.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import {Notice,Footer,NavBar,SplitLine} from '.';
 import { FlexDirectionProperty } from 'csstype';
 export interface AppLayoutProps{
     enableNoticeComponent?:boolean;
-    type?:"plain" | "std";
+    type?:"plain" | "std" | "no-container";
 }
 export class AppLayout extends React.Component<AppLayoutProps>{
     readonly outerStyle = {
@@ -11,7 +11,7 @@ export class AppLayout extends React.Component<AppLayoutProps>{
     }
     readonly wrapperStyle={
         height:'100%',
-        marginTop:'50px',
+        paddingTop:'50px',
         backgroundColor:'white',
         display: 'flex',
         flexDirection: "column" as FlexDirectionProperty
@@ -20,14 +20,33 @@ export class AppLayout extends React.Component<AppLayoutProps>{
         padding:'10px',
         flex: '1 0 auto'
     }
+    readonly rawContainerStyle={
+        flex: '1 0 auto'
+    }
     render(){
         switch(this.props.type){
             case "plain":
                 return this.renderPlain();
+            case "no-container":
+                return this.renderNoContainer();
             case "std":
             default:
                 return this.renderStd();
         }
+    }
+    private renderNoContainer(){
+        return (<div style={{height:'100%'}}>
+            <NavBar></NavBar>
+            <div style={this.wrapperStyle}>
+                <div style={this.rawContainerStyle}>
+                    {(this.props.enableNoticeComponent || this.props.enableNoticeComponent == null) &&
+                        <Notice></Notice>
+                    }
+                    {this.props.children}
+                </div>
+                <Footer></Footer>
+            </div>
+        </div>)
     }
     private renderPlain(){
         return <div>{this.props.children}</div>
--- src/view/rpage/index/index.tsx
+++ src/view/rpage/index/index.tsx
@@ -2,26 +2,26 @@ require("./app.css");
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Notice, NavBar, Footer,AppLayout} from '../../controls';
+import { url } from 'inspector';
+const img = require('./headbg.jpg');
 require('./index.css');
 class App extends React.Component{
     render(){
         return(
-            <AppLayout type="plain">
-                    <NavBar></NavBar>
-                    <Header></Header>
-                    <div>
-                        <div className="container">
-                            <Notice></Notice>
-                        </div>
-                    </div>
-                    <Footer></Footer>
+            <AppLayout type="no-container" enableNoticeComponent={false}>
+                <Header></Header>
+                <div className="container">
+                    <Notice></Notice>
+                </div>
             </AppLayout>)
     }
 }
 class Header extends React.Component{
     render(){
-        return <div>
-            <h1>Fuck</h1>
+        return <div id="header">
+            <div className="container jumbotron" style={{background:'transparent'}}>
+                <h1>Wow</h1>
+            </div>
         </div>
     }
 }
