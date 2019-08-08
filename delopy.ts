ebbef1f1f32ba5e5058a5fef82a266a101b31a8b
zsh2401 <zsh2401@163.com>
2019-08-08 12:25:38 +0800

    'a'

--- delopy.ts
+++ delopy.ts
@@ -2,4 +2,4 @@ import {execSync} from 'child_process'
 console.log(execSync("cd _dist_",{encoding:"utf-8"}));
 console.log(execSync("git add .",{encoding:"utf-8"}));
 console.log(execSync("git commit -m 'a'",{encoding:"utf-8"}));
-console.log(execSync("git push oschina -f",{encoding:"utf-8"}))
+console.log(execSync("git push -f",{encoding:"utf-8"}))
